4c64c28f5405f4fa789be77f6188b95770cd4e3e
grazy-github <graziela-gloria@hotmail.com>
2021-12-23 00:52:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos_de_variavel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Ana ";
+            string lastname = "Oliveira";
+            string fullname = firstname + lastname;
+            int age = 23;
+
+            Console.WriteLine("\n Meu nome completo é {0}", fullname + ". \n"
+                + "\n Minha idade é {0}", age
+                );
+            //dont use a lot of lines with code if you can do it better
+        }
+    }
+}
